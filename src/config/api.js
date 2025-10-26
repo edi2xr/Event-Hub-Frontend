8b369e0ba84e83ece65a2b8949324bfe2059f04f
Mathew <mathewkariuki2005@gmail.com>
2025-10-27 01:37:21 +0300

    modified the url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || " https://cationic-nonhabitually-joella.ngrok-free.dev";
 
 export const API_ENDPOINTS = {
   auth: {
